fceb0cfac87e6586d06baf5b35f64f4fc151793d
ksi <ksi@confident.ru>
2023-06-14 10:23:19 +0300

    Доработка VMwareUser

--- VMwareUser/main.py
+++ VMwareUser/main.py
@@ -3,9 +3,22 @@ import getpass
 import json
 import os
 import time
+from enum import Enum
 from pyVim.connect import SmartConnectNoSSL, SmartConnect  # подключение
 from pyVmomi import vim  # список виртуальных
 from termcolor import colored
+
+
+class PowerTaskId(Enum):
+    """Команда на вкл/выкл машины"""
+    on = 1
+    off = 2
+    suspend = 3
+
+class SnapshotByNameTaskId(Enum):
+    """Команда на поиск снапшота и выполнение задачи к нему"""
+    revert = 1
+    remove = 2
 
 
 class VMwareUser:
@@ -14,10 +27,11 @@ class VMwareUser:
 
     def __init__(self):
         self.connection = None  # подклчючение
-        self.vsphere_machines = {}  # список имён считанных машин
-        self.apply_machines = []  # список имён машин,с которыми будут проводиться работы
+        self.vsphere_machines = {}  # список атрибутов считанных машин
+        self.available_apply_machines = {}  # cловарь словарей машин для обработки
+        self.apply_machine_list_name = ""  # имя выбранного списка машин
+        self.apply_machine_names = []  # список имён машин,с которыми будут проводиться работы
         self.async_loop = asyncio.new_event_loop()  # цикл обработки асинхронных заданий
-        self.available_apply_machines = {}  # cловарь доступных списков машин для обработки
 
     def __del__(self):
         self.async_loop.close()
@@ -67,8 +81,8 @@ class VMwareUser:
         print("Выберите номер списка машин, с которым планируется проводить работы:")
         for _ in range(5):
             count = 0
-            for name, machine_names in self.available_apply_machines.items():
-                print(str(count), ") ", name, " - ", str(len(machine_names)), ' машин')
+            for name, machine_dicts in self.available_apply_machines.items():
+                print(str(count), ") ", name, " - ", str(len(machine_dicts)), ' машин')
                 count += 1
 
             try:
@@ -84,34 +98,41 @@ class VMwareUser:
         if cmd == None:
             raise Exception("Список машин для обработки не выбран")
 
-        for name, machine_names in self.available_apply_machines.items():
+        for name, machine_dicts in self.available_apply_machines.items():
             if cmd == 0:
                 # Проверка, что все машины в списке есть на VSphere
                 vsphere_machine_names = [vsphere_machine[0] for vsphere_machine in self.vsphere_machines]
+                machine_names_found = []
                 machine_names_not_found = []
-                for machine_name in machine_names:
-                    if machine_name not in vsphere_machine_names:
+                for machine_dict in machine_dicts:
+                    machine_name = machine_dict["vm_name"]
+                    if machine_name in vsphere_machine_names:
+                        machine_names_found.append(machine_name)
+                    else:
                         machine_names_not_found.append(machine_name)
                 if machine_names_not_found:
                     raise Exception(
                         f"В загруженном списке машин обнаружены имена, которых нет на сервере VSphere: \n {machine_names_not_found}")
-                self.apply_machines = machine_names
+                self.apply_machine_names = machine_names_found
+                self.apply_machine_list_name = name
                 break
             else:
                 cmd -= 1
-        print('Для обработки выбрано ', colored(str(len(self.apply_machines)), 'magenta'), ' виртуальных машин')
+        print('Для обработки выбрано ', colored(str(len(self.apply_machine_names)), 'magenta'), ' виртуальных машин')
 
     def machine_generator(self):
         '''Генератор машин для обработки'''
         return ((machine_name, machine_ref) for machine_name, machine_ref in self.vsphere_machines
-                if machine_name in self.apply_machines)
+                if machine_name in self.apply_machine_names)
 
     def shapshot_generator(self, vm_machine):
         '''Генератор рекурсивного обхода снапшотов'''
+
         def recursive_yield_child_snapshots(shapshots):
             for shapshot in shapshots:
                 yield shapshot
                 yield from recursive_yield_child_snapshots(shapshot.childSnapshotList)
+
         yield from recursive_yield_child_snapshots(vm_machine.snapshot.rootSnapshotList)
 
     def wait_for_tasks(self, tasks):
@@ -130,21 +151,41 @@ class VMwareUser:
         print(colored('Выполнение ', 'grey'), colored(str(len(tasks)), 'magenta'),
               colored(' задач завершено', 'grey'))
 
-    def revert_to_snapshot_name(self):
-        '''Откатить машины к снапшоту по указанному имени снапшота'''
+    def snapshot_by_name_task(self, snapshot_by_name_task_id: SnapshotByNameTaskId):
+        '''Задание к снапшоту с указанным именем'''
         snapshot_name = str(input("Введите имя снапшота: "))
         tasks = []
         for name, vm in self.machine_generator():
             snapshot_not_found = True
             for snapshot in self.shapshot_generator(vm):
                 if snapshot.name == snapshot_name:
-                    tasks.append(snapshot.snapshot.RevertToSnapshot_Task())
-                    print("Откат машины", colored(name, 'green'), "к снапшоту", colored(snapshot_name, 'blue'))
-                    snapshot_not_found = False
-                    break
+                    # Откат к первому найденному снапшоту
+                    if snapshot_by_name_task_id == SnapshotByNameTaskId.revert:
+                        tasks.append(snapshot.snapshot.RevertToSnapshot_Task())
+                        print("Откат к снапшоту", colored(snapshot_name, 'blue'), "машины", colored(name, 'green'))
+                        snapshot_not_found = False
+                        break
+                    # Удаление всех снапшотов с указанным именем
+                    elif snapshot_by_name_task_id == SnapshotByNameTaskId.remove:
+                        tasks.append(snapshot.snapshot.RemoveSnapshot_Task(removeChildren=True))
+                        print("Удаление снапшота", colored(snapshot_name, 'blue'), "машины", colored(name, 'green'))
+                        snapshot_not_found = False
+                        continue
+                    else:
+                        raise Exception("Неизвестная команда для работы со снапшотом")
             if snapshot_not_found:
                 print("Для машины", colored(name, 'green'), "снапшот с именем", colored(snapshot_name, 'blue'),
                       colored("не найден", 'red'))
+        # Ожидание завершения всех заданий
+        self.wait_for_tasks(tasks)
+
+    def create_snapshot_name(self):
+        '''Создать снапшоту с указанным именем'''
+        snapshot_name = str(input("Введите имя создаваемого снапшота: "))
+        tasks = []
+        for name, vm in self.machine_generator():
+            tasks.append(vm.CreateSnapshot_Task(name=snapshot_name, memory=True, quiesce=True))
+            print("Создание снапшота", colored(snapshot_name, 'blue'), "для машины", colored(name, 'green'))
         # Ожидание завершения всех заданий
         self.wait_for_tasks(tasks)
 
@@ -158,18 +199,33 @@ class VMwareUser:
         # Ожидание завершения всех заданий
         self.wait_for_tasks(tasks)
 
-    def power_on_off(self, power_on: bool = True):
+    def power_task(self, power_task_id: PowerTaskId):
         '''Включение/выключение машин'''
         tasks = []
         for name, vm in self.machine_generator():
-            if power_on:
+            if power_task_id == PowerTaskId.on:
                 tasks.append(vm.PowerOnVM_Task())
                 print("Включение машины", colored(name, 'green'))
-            else:
+            elif power_task_id == PowerTaskId.off:
                 tasks.append(vm.PowerOffVM_Task())
                 print("Выключение машины", colored(name, 'green'))
+            elif power_task_id == PowerTaskId.suspend:
+                tasks.append(vm.SuspendVM_Task())
+                print("Приостановка машины", colored(name, 'green'))
+            else:
+                raise Exception("Неизвестная команда для работы с питанием машины")
         # Ожидание завершения всех заданий
         self.wait_for_tasks(tasks)
+
+    def create_remote_installer_file(self):
+        file_data = ""
+        for machine_dict in self.available_apply_machines[self.apply_machine_list_name]:
+            file_data += machine_dict["hostname"] + "\n"
+        file_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "remote_installer_list.txt")
+        file = open(file_path, "w", encoding="utf-8")
+        file.write(file_data)
+        file.close()
+        print("Список сохранен в файл", colored(file_path, 'green'))
 
 
 if __name__ == '__main__':
@@ -191,10 +247,14 @@ if __name__ == '__main__':
         print(colored("==================================================", "grey"))
         cmd = input("""Введите номер команды:
 0 - выход
-1 - откатить машины к снапшоту по указанному имени снапшота
-2 - откатить машины к последнему снапшоту
+1 - откатить к снапшоту с указанным именем
+2 - создать снапшот с указанным именем
+3 - удалить снапшоты с указанным именем
 4 - включить машины
 5 - выключить машины
+6 - приостановить машины
+7 - откатить машины к последнему снапшоту
+9 - подготовить файл со списком машин для удаленного развертывания
 >> """
                     )
         try:
@@ -202,13 +262,21 @@ if __name__ == '__main__':
             if cmd == 0:
                 break
             elif cmd == 1:
-                wmware_user.revert_to_snapshot_name()
+                wmware_user.snapshot_by_name_task(SnapshotByNameTaskId.revert)
             elif cmd == 2:
-                wmware_user.revert_to_last_snapshot()
+                wmware_user.create_snapshot_name()
+            elif cmd == 3:
+                wmware_user.snapshot_by_name_task(SnapshotByNameTaskId.remove)
             elif cmd == 4:
-                wmware_user.power_on_off(True)
+                wmware_user.power_task(PowerTaskId.on)
             elif cmd == 5:
-                wmware_user.power_on_off(False)
+                wmware_user.power_task(PowerTaskId.off)
+            elif cmd == 6:
+                wmware_user.power_task(PowerTaskId.suspend)
+            elif cmd == 7:
+                wmware_user.revert_to_last_snapshot()
+            elif cmd == 9:
+                wmware_user.create_remote_installer_file()
             else:
                 raise Exception("Неверный номер команды")
 
